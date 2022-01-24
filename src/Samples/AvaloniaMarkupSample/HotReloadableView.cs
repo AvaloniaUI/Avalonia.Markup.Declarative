adf92d1b24fb77a8f1185c5f31e50c5a05e3e707
Igor Gritsenko <igor@gritsenko.biz>
2022-01-24 14:16:37 +0300

    Udated sampe with templated custom control

--- src/Samples/AvaloniaMarkupSample/HotReloadableView.cs
+++ src/Samples/AvaloniaMarkupSample/HotReloadableView.cs
@@ -1,4 +1,7 @@
-﻿public class HotReloadView : ViewBase
+﻿using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml.Templates;
+
+public class HotReloadView : ViewBase
 {
     protected override object Build() =>
         new StackPanel()
@@ -18,8 +21,30 @@
                     .BorderBrush(Brushes.Gray)
                     .BorderThickness(1)
                     .Child(
-                    new MyCustomControl()
-                        .Margin(20)
-                    )
+                        new MyCustomControl()
+                            .Margin(20)
+                    ),
+
+                new TextBlock()
+                    .Text("Custom templated control example:"),
+
+                new Border()
+                    .BorderBrush(Brushes.Gray)
+                    .BorderThickness(1)
+                    .Child(
+                        new MyCustomTemplatedControl()
+                            .Ref(out var myCustomTemplatedControl)
+                            .Margin(20)
+                            .Template(StaticResources.Templates.MyControlTemplate)
+                    ),
+                new ToggleButton()
+                    .Ref(out var tb)
+                    .Content("Change template")
+                    .OnClick(args =>
+                    {
+                        myCustomTemplatedControl.Template = !(tb.IsChecked ?? false)
+                            ? StaticResources.Templates.MyControlTemplate
+                            : StaticResources.Templates.MyAnotherControlTemplate;
+                    })
             );
 }
---
+++ src/Samples/AvaloniaMarkupSample/StaticResources.cs
@@ -0,0 +1,78 @@
+﻿using Avalonia.Controls.Templates;
+
+public static class StaticResources
+{
+    public static class Templates
+    {
+        public static IControlTemplate MyControlTemplate { get; } = new FuncControlTemplate<MyCustomTemplatedControl>(
+            (control, scope) =>
+                new Grid()
+                    .Rows("Auto, Auto, *, Auto")
+                    .Children(
+                        new StackPanel()
+                            .Orientation(Orientation.Horizontal)
+                            .VerticalAlignment(VerticalAlignment.Center)
+                            .Children(
+                                new TextBlock().Text("Enter text:")
+                                    .VerticalAlignment(VerticalAlignment.Center),
+                                new TextBox()
+                                    .Text(@control.NewValue, BindingMode.TwoWay, bindingSource: control)
+                                    .MinWidth(150)
+                            ),
+                        new StackPanel().Row(1)
+                            .Margin(0, 10)
+                            .Orientation(Orientation.Horizontal)
+                            .VerticalAlignment(VerticalAlignment.Center)
+                            .HorizontalAlignment(HorizontalAlignment.Left)
+                            .Children(
+                                new TextBlock().Text("Saved text:")
+                                    .VerticalAlignment(VerticalAlignment.Center),
+                                new TextBox()
+                                    .Text(@control.SavedValue, BindingMode.TwoWay, bindingSource: control)
+                                    .MinWidth(150)
+                            ),
+                        new StackPanel().Row(3)
+                            .Orientation(Orientation.Horizontal)
+                            .HorizontalAlignment(HorizontalAlignment.Right)
+                            .Children(
+                                new Button().Content("Cancel")
+                                    .Margin(5, 0)
+                                    .IsEnabled(@control.CanSave, bindingSource: control)
+                                    .Command(new Binding(nameof(MyCustomTemplatedControl.Cancel)) {Source = control}),
+                                new Button().Content("Save")
+                                    .Margin(5, 0)
+                                    .IsEnabled(@control.CanSave, bindingSource: control)
+                                    .Command(new Binding(nameof(MyCustomTemplatedControl.Save)) {Source = control})
+                            )
+                    ));
+
+        public static IControlTemplate MyAnotherControlTemplate { get; } =
+            new FuncControlTemplate<MyCustomTemplatedControl>((control, scope) =>
+                new StackPanel()
+                    .Children(
+                        new StackPanel()
+                            .Orientation(Orientation.Horizontal)
+                            .VerticalAlignment(VerticalAlignment.Center)
+                            .Children(
+                                new TextBlock().Text("Entered text:")
+                                    .VerticalAlignment(VerticalAlignment.Center),
+                                new TextBlock()
+                                    .Text(@control.NewValue, bindingSource: control)
+                                    .MinWidth(150)
+                            ),
+                        new StackPanel()
+                            .Margin(0, 10)
+                            .Orientation(Orientation.Horizontal)
+                            .VerticalAlignment(VerticalAlignment.Center)
+                            .HorizontalAlignment(HorizontalAlignment.Left)
+                            .Children(
+                                new TextBlock().Text("Saved text:")
+                                    .VerticalAlignment(VerticalAlignment.Center),
+                                new TextBlock()
+                                    .Text(@control.SavedValue, bindingSource: control)
+                                    .MinWidth(150)
+                            )
+                    ));
+
+    }
+}
