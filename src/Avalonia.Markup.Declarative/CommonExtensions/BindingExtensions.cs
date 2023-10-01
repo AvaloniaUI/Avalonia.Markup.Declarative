cb38952f37407f611195d0b9e85612ab7cbdb8de
Igor Gritsenko <igor@gritsenko.biz>
2023-10-02 00:29:21 +0300

    folder structure update, hot reload fix

---
+++ src/Avalonia.Markup.Declarative/CommonExtensions/BindingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+
+namespace Avalonia.Markup.Declarative;
+
+public static class BindingExtensions
+{
+    public static TBinding Converter<TBinding>(this TBinding binding, IValueConverter converter)
+        where TBinding : Binding
+    {
+        binding.Converter = converter;
+        return binding;
+    }
+
+}
