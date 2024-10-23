9e6bb63e1cbf090faec7196ebd4a5225a6c5054b
Weihan Li <7604648+WeihanLi@users.noreply.github.com>
2024-10-23 08:23:38 +0800

    use nameof for generator

--- src/AvaloniaExtensionGenerator/Generators/AttachedPropertySetterGenerator/AttachedPropertyBindFromExpressionSetterGenerator.cs
+++ src/AvaloniaExtensionGenerator/Generators/AttachedPropertySetterGenerator/AttachedPropertyBindFromExpressionSetterGenerator.cs
@@ -5,7 +5,7 @@ namespace AvaloniaExtensionGenerator.Generators.AttachedPropertySetterGenerator;
 public class AttachedPropertyBindFromExpressionSetterGenerator : ExtensionGeneratorBase<AttachedPropertyExtensionInfo>
 {
     protected override string? GetExtension(AttachedPropertyExtensionInfo info) =>
-        $"public static {info.ReturnType} {info.ExtensionName}{info.GenericArg}(this {info.ReturnType} control, Func<{info.ValueTypeSource}> func, Action<{info.ValueTypeSource}>? onChanged = null, [CallerArgumentExpression(\"func\")] string? expression = null)" +
+        $"public static {info.ReturnType} {info.ExtensionName}{info.GenericArg}(this {info.ReturnType} control, Func<{info.ValueTypeSource}> func, Action<{info.ValueTypeSource}>? onChanged = null, [CallerArgumentExpression(nameof(func))] string? expression = null)" +
         $" where T : {info.AttachedPropertyHostTypeName} {Environment.NewLine}" +
         $"   => control._set({info.ControlTypeName}.{info.FieldInfo.Name}, func, onChanged, expression);";
 
--- src/AvaloniaExtensionGenerator/Generators/SetterGenerators/BindFromExpressionSetterGenerator.cs
+++ src/AvaloniaExtensionGenerator/Generators/SetterGenerators/BindFromExpressionSetterGenerator.cs
@@ -5,7 +5,7 @@ namespace AvaloniaExtensionGenerator.Generators.SetterGenerators;
 public class BindFromExpressionSetterGenerator : ExtensionGeneratorBase<PropertyExtensionInfo>
 {
     protected override string? GetExtension(PropertyExtensionInfo info) =>
-        $"public static {info.ReturnType} {info.ExtensionName}{info.GenericArg}(this {info.ReturnType} control, Func<{info.ValueTypeSource}> func, Action<{info.ValueTypeSource}>? onChanged = null, [CallerArgumentExpression(\"func\")] string? expression = null) {info.GenericConstraint} {Environment.NewLine}" +
+        $"public static {info.ReturnType} {info.ExtensionName}{info.GenericArg}(this {info.ReturnType} control, Func<{info.ValueTypeSource}> func, Action<{info.ValueTypeSource}>? onChanged = null, [CallerArgumentExpression(nameof(func))] string? expression = null) {info.GenericConstraint} {Environment.NewLine}" +
         $"   => control._set({info.ControlTypeName}.{info.FieldInfo.Name}, func, onChanged, expression);";
 
 }
