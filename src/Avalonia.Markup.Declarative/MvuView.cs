67439a61ea122fafb6c5485723e73a1c7de8024b
HUFEI\hufei-m <hufei@geblab.com>
2025-04-11 14:17:52 +0800

    + Add MvuView for better performance

---
+++ src/Avalonia.Markup.Declarative/MvuView.cs
@@ -0,0 +1,103 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+
+namespace Avalonia.Markup.Declarative;
+
+public abstract class MvuView : ViewBase, IMvuComponent
+{
+    private List<IMvuComponent>? _observerViews;
+
+    public MvuView()
+        : base()
+    {
+    }
+
+    protected override void OnCreated()
+    {
+        InitializeState();
+    }
+
+    protected virtual void InitializeState()
+    {
+    }
+
+    public void UpdateState(Action? updateStateAction = default)
+    {
+        updateStateAction?.Invoke();
+        StateHasChanged();
+    }
+
+    internal Action? setStateAction;
+
+    protected virtual void StateHasChanged()
+    {
+        if (setStateAction != null) setStateAction();
+
+        if (_observerViews != null)
+            foreach (var view in _observerViews)
+                view.UpdateState();
+
+        foreach (var computedState in __viewComputedStates)
+            computedState.OnPropertyChanged();
+    }
+
+    public static Control[] BuildControls(params Control[] controls)
+    {
+        var list = new List<Control>(controls.Length);
+        foreach (var control in controls)
+        {
+            if (control != null)
+                list.Add(control);
+        }
+        return list.ToArray();
+    }
+
+    public void AddObserverView(IMvuComponent view)
+    {
+        _observerViews ??= new List<IMvuComponent>();
+
+        if (!_observerViews.Contains(view))
+            _observerViews.Add(view);
+    }
+
+    public void RemoveObserverView(IMvuComponent view)
+    {
+        _observerViews?.Remove(view);
+    }
+
+    public void ClearObserverViews()
+    {
+        _observerViews?.Clear();
+    }
+}
+
+public static class MvuViewExtensions
+{
+    public static TViewModel Observe<TViewModel>(this TViewModel targetView, MvuView view)
+        where TViewModel : MvuView
+    {
+        view.AddObserverView(targetView);
+        return targetView;
+    }
+
+    public static TViewModel RemoveObserve<TViewModel>(this TViewModel targetView, MvuView view)
+    where TViewModel : MvuView
+    {
+        view.RemoveObserverView(targetView);
+        return targetView;
+    }
+
+    public static TViewModel SetState<TViewModel>(this TViewModel targetView, Action<TViewModel> action, bool setOnce = true)
+        where TViewModel : MvuView
+    {
+        if (setOnce == false)
+            targetView.setStateAction = () => { action(targetView); };
+        else
+            targetView.setStateAction = null;
+
+        action.Invoke(targetView);
+        targetView.UpdateState();
+        return targetView;
+    }
+}
---
+++ src/Samples/AvaloniaMarkupSample/MvuSample/AnotherMvuView.cs
@@ -0,0 +1,56 @@
+﻿namespace AvaloniaMarkupSample.MvuSample;
+
+internal class CounterState
+{
+    public readonly static CounterState Default = new CounterState();
+    public int Counter { get; set; } = 0;
+}
+
+public class AnotherMvuView : MvuView
+{
+    protected override object Build() =>
+    new StackPanel().Spacing(10).HorizontalAlignment(HorizontalAlignment.Center)
+            .Children(
+                     new TextBlock()
+                            .Text(() => $"[AnotherMvuView] Counter: {(State.Counter == 0 ? "zero" : State.Counter)}"),
+                     new EmbedMvuView()
+                            .SetState(x => { x.State = State; })
+                            .Observe(this),
+                     new EmbedMvuView()
+                            .SetState(x => { x.State = State; }, false)
+                            .Observe(this),
+                     new Button().IsVisible(() => State.Counter < 5)
+                                .Content("Click me")
+                                .OnClick(OnButtonClick)
+            );
+
+    internal CounterState State { get; set; } = new CounterState();
+
+    private void OnButtonClick(RoutedEventArgs e)
+    {
+        State.Counter++;
+        int count = State.Counter;
+
+        this.State = new CounterState() { Counter = count };
+
+        if (State.Counter >= 5)
+        {
+            this.ClearObserverViews();
+            this.Reload();
+        }
+        else
+            StateHasChanged();
+    }
+}
+
+public class EmbedMvuView : MvuView
+{
+    protected override object Build() =>
+        new StackPanel()
+            .Children(
+                new TextBlock()
+                    .Text(() => $"[EmbedMvuView] Counter: {(State.Counter == 0 ? "zero" : State.Counter)}")
+            );
+
+    internal CounterState State { get; set; } = CounterState.Default;
+}
