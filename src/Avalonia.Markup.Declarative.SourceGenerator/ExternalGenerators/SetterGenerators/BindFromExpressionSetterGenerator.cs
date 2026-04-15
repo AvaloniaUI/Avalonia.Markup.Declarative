0e8f7dc4f4691b68be4a352320a61c040a5727a2
Igor Gritsenko <igor@gritsenko.biz>
2026-04-15 19:38:39 +0300

    removed statless varion of epression setters

--- src/Avalonia.Markup.Declarative.SourceGenerator/ExternalGenerators/SetterGenerators/BindFromExpressionSetterGenerator.cs
+++ src/Avalonia.Markup.Declarative.SourceGenerator/ExternalGenerators/SetterGenerators/BindFromExpressionSetterGenerator.cs
@@ -11,9 +11,9 @@ internal sealed class BindFromExpressionSetterGenerator : ExtensionGeneratorBase
         var withSource = $"public static {info.ReturnType} {info.ExtensionName}{genericParams}(this {info.ReturnType} control, TViewModel source, Expression<Func<TViewModel, TValue>> getter, BindingMode? mode = null, IValueConverter? converter = null{CallerInfoParameters}) {info.GenericConstraint} {SymbolUtilities.NewLine}" +
                          $"   => control._setCompiledBinding({info.ControlTypeName}.{info.FieldSymbol.Name}!, source, getter, mode, converter{CallerInfoArguments});";
 
-        var withoutSource = $"public static {info.ReturnType} {info.ExtensionName}{genericParams}(this {info.ReturnType} control, Expression<Func<TViewModel, TValue>> getter, BindingMode? mode = null, IValueConverter? converter = null{CallerInfoParameters}) {info.GenericConstraint} {SymbolUtilities.NewLine}" +
-                            $"   => control._setCompiledBinding({info.ControlTypeName}.{info.FieldSymbol.Name}!, getter, mode, converter{CallerInfoArguments});";
+        //var withoutSource = $"public static {info.ReturnType} {info.ExtensionName}{genericParams}(this {info.ReturnType} control, Expression<Func<TViewModel, TValue>> getter, BindingMode? mode = null, IValueConverter? converter = null{CallerInfoParameters}) {info.GenericConstraint} {SymbolUtilities.NewLine}" +
+        //                    $"   => control._setCompiledBinding({info.ControlTypeName}.{info.FieldSymbol.Name}!, getter, mode, converter{CallerInfoArguments});";
 
-        return withSource + SymbolUtilities.NewLine + withoutSource;
+        return withSource; // + SymbolUtilities.NewLine + withoutSource;
     }
 }
